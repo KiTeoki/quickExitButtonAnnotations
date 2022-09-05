0ea08c75a0669e0eef5f7ed56c46ba1620b78e5b
Kieron Ivy Turk <kieron.turk@gmail.com>
2022-09-05 23:08:46 +0100

    Fix chi2 tests
    Replace 3 variable chi2 test (country x category x presence) with pair of 2 variable tests

--- significance_tests.py
+++ significance_tests.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import numpy as np
 from math import sqrt
 import scipy.stats as stats
+import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.graphics.mosaicplot import mosaic
 
@@ -13,14 +14,13 @@ categories = ['Domestic Abuse', 'Rape/SA', 'LGBTQ+', 'BAME(R)', 'Sobreity', 'Smo
               'Family Planning', 'Parenting', 'Children', 'Homelessness', 'Sexual health', 'Mental Health',
               'Physical Health', 'Disability', 'Elderly', 'Past offenders', 'Victims', 'Police', 'Misc']
 collapsed_categories = ['Gendered Violence', 'Minorities', 'Addiction', 'Families', 'Healthcare', 'Crime', 'Misc']
+presence = ['Present', 'Absent']
 
 def count_mechanisms(platform, mechanism, use_collapsed_categories=False):
     selected_categories = collapsed_categories if use_collapsed_categories else categories
     # Count times mechanism was present on platform
-    nsites = {(country, category): 0 for country in countries for category in selected_categories}
-    counts = {(country, category): 0 for country in countries for category in selected_categories}
-    country_counts = {country: 0 for country in countries}
-    category_counts = {category: 0 for category in selected_categories}
+    country_counts = {(country,present): 0 for country in countries for present in presence}
+    category_counts = {(category,present): 0 for category in selected_categories for present in presence}
     total = 0
     with open(sitelist_filename, "r") as f:
         reader = csv.DictReader(f)
@@ -40,53 +40,76 @@ def count_mechanisms(platform, mechanism, use_collapsed_categories=False):
                     category = 'Misc'
 
             country = site['Region']
-            nsites[(country,category)]+=1
-            if ((platform == 'Desktop' and mechanism == 'Button' and site['Desktop Exit Button?'] != '') or
+            present = ((platform == 'Desktop' and mechanism == 'Button' and site['Desktop Exit Button?'] != '') or
                 (platform == 'Desktop' and mechanism == 'Shortcut' and site['Keyboard shortcut type'] != '') or
                 (platform == 'Mobile' and mechanism == 'Button' and site['Mobile Exit button?'] != '') or
-                (platform == 'Mobile' and mechanism == 'Shortcut' and site['Mobile shortcut?'] != '')):
-                    country_counts[country]+=1; category_counts[category]+=1; counts[(country, category)]+=1; total += 1
+                (platform == 'Mobile' and mechanism == 'Shortcut' and site['Mobile shortcut?'] != ''))
+            country_counts[country,presence[1-present]]+=1
+            category_counts[category,presence[1-present]]+=1
+            if present:
+                total += 1
 
-    return counts, country_counts, category_counts, nsites, total
+    return country_counts, category_counts, total
 
 
-def chi_squared_test(platform, mechanism, use_collapsed_categories=False):
+def chi_squared_tests(platform, mechanism, use_collapsed_categories=False):
     selected_categories = collapsed_categories if use_collapsed_categories else categories
-    counts, country_counts, category_counts, nsites, total = count_mechanisms(platform, mechanism, use_collapsed_categories)
+    country_counts, category_counts, total = count_mechanisms(platform, mechanism, use_collapsed_categories)
     residuals = {}
     likelihood_mechanism = total / 2045.0
-    chi2 = 0
+    chi2_country = 0
     for i, country in enumerate(countries):
-        for j, category in enumerate(selected_categories):
-            e_ij = likelihood_mechanism * nsites[(country,category)] # float(country_counts[country] * category_counts[category]) / float(total)
-            r_ij = float(counts[(country,category)] - e_ij)/sqrt(e_ij) if e_ij != 0 else 0
-            residuals[(country, category)] = r_ij
-            chi2 += r_ij**2
+        # Entry for presence in country
+        e_ij = likelihood_mechanism * (country_counts[country,'Present'] + country_counts[country,'Absent'])
+        r_ij = float(country_counts[country,'Present'] - e_ij)/sqrt(e_ij) if e_ij != 0 else 0
+        residuals[country,'Present'] = r_ij
+        chi2_country += r_ij**2
+        # Entry for absence in country
+        e_ij = (1-likelihood_mechanism) * (country_counts[country,'Present'] + country_counts[country,'Absent'])
+        r_ij = float(country_counts[country,'Absent'] - e_ij) / sqrt(e_ij) if e_ij != 0 else 0
+        residuals[country,'Absent'] = r_ij
+        chi2_country += r_ij ** 2
     dof = (len(countries) - 1) * (len(selected_categories) - 1)
-    p = 1 - stats.chi2.cdf(chi2, dof)
-    draw_mosaic(counts, residuals, platform, mechanism)
-    return chi2, dof, p
+    p_country = 1 - stats.chi2.cdf(chi2_country, dof)
+    draw_mosaic(country_counts, residuals, platform, mechanism)
+
+    chi2_category = 0
+    for j, category in enumerate(selected_categories):
+        # presence within category
+        e_ij = likelihood_mechanism * (category_counts[category,'Present'] + category_counts[category,'Absent'])
+        r_ij = float(category_counts[category,'Present'] - e_ij)/sqrt(e_ij) if e_ij != 0 else 0
+        residuals[category,'Present'] = r_ij
+        chi2_category += r_ij**2
+        # absence within category
+        e_ij = (1-likelihood_mechanism) * (category_counts[category,'Present'] + category_counts[category,'Absent'])
+        r_ij = float(category_counts[category,'Absent'] - e_ij) / sqrt(e_ij) if e_ij != 0 else 0
+        residuals[category,'Absent'] = r_ij
+        chi2_category += r_ij ** 2
+    p_category = 1 - stats.chi2.cdf(chi2_country, dof)
+    draw_mosaic(category_counts, residuals, platform, mechanism)
+
+    return chi2_country, chi2_category, dof, min(p_country, 1-p_country), min(p_category, 1-p_category)
 
 def draw_mosaic(counts, residuals, platform, mechanism):
-    # TODO derive thresholds based on p values of residuals
     props = lambda k: {'color': 'red' if residuals[k] > 4 else
                                 'orange' if residuals[k] > 2 else
                                 'yellow' if residuals[k] > 0.5 else
-                                'light blue' if residuals[k] < -4 else
+                                'darkblue' if residuals[k] < -4 else
                                 'blue' if residuals[k] < -2 else
-                                'darkblue' if residuals[k] < -0.5 else
+                                'lightblue' if residuals[k] < -0.5 else
                                 'grey'
                        }
-    labeliser = lambda k: "\n".join(k) if abs(residuals[k])>4 else ''
-    mosaic(counts, gap=0.01, properties=props, labelizer=labeliser, title='Presence of '+platform+' '+mechanism)
+    labeliser = lambda k: ''#"\n".join(k) if abs(residuals[k])>4 else ''
+    mosaic(counts, gap=0.01, properties=props, labelizer=labeliser, label_rotation=(20,0,0,0),
+           title='Presence of '+platform+' '+mechanism)
     plt.show()
 
 if __name__ == '__main__':
-    print(chi_squared_test('Desktop', 'Button'))
-    #print(chi_squared_test('Desktop', 'Shortcut'))
-    print(chi_squared_test('Mobile', 'Button'))
-    #print(chi_squared_test('Mobile', 'Shortcut'))
-    print(chi_squared_test('Desktop', 'Button', True))
-    #print(chi_squared_test('Desktop', 'Shortcut', True))
-    print(chi_squared_test('Mobile', 'Button', True))
-    #print(chi_squared_test('Mobile', 'Shortcut', True))
+    print(chi_squared_tests('Desktop', 'Button'))
+    #print(chi_squared_tests('Desktop', 'Shortcut'))
+    print(chi_squared_tests('Mobile', 'Button'))
+    #print(chi_squared_tests('Mobile', 'Shortcut'))
+    print(chi_squared_tests('Desktop', 'Button', True))
+    #print(chi_squared_tests('Desktop', 'Shortcut', True))
+    print(chi_squared_tests('Mobile', 'Button', True))
+    #print(chi_squared_tests('Mobile', 'Shortcut', True))
